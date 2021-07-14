abdc983545cd29e32a773970b52d19703ae2b9f6
ZUIcat <35529568+ZUIcat@users.noreply.github.com>
2021-07-14 20:36:40 +0800

    none

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main() {
     MyRenderWindow *myRenderWindow = new MyRenderWindow("LearnOpenGL", 800, 600);
     myRenderWindow->InitWindow();
     myRenderWindow->StartDraw();
-    delete(myRenderWindow);
+    delete (myRenderWindow);
     std::cout << "[APP END]" << std::endl;
     return 0;
 }
